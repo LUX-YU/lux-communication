087c46acb8f88712c618c2378939249d97e6f418
chenhui.lux.yu <chenhui.lux.yu@outlook.com>
2024-02-26 04:54:07 +0000

    Updated.

--- builtin_msgs/include/lux/communication/builtin_msgs/timestamp_tools.hpp
+++ builtin_msgs/include/lux/communication/builtin_msgs/timestamp_tools.hpp
@@ -12,4 +12,10 @@ namespace lux::communication::builtin_msgs
 	LUX_COMMUNICATION_PUBLIC void timestamp_from_ns(common_msgs::Timestamp& timestamp, uint64_t time_ns);
 
 	LUX_COMMUNICATION_PUBLIC uint64_t timestamp_to_ns(const common_msgs::Timestamp& timestamp);
+
+	template<typename T>
+	uint64_t message_get_ns_ts(const T& message)
+	{
+		return timestamp_to_ns(message.timestamp());
+	}
 }
