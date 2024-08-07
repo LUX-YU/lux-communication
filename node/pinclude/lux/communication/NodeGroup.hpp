beeed3ec7dd74c182685cc3c4e57f8bcf9d9239d
chenhui.lux.yu <chenhui.lux.yu@outlook.com>
2024-08-07 01:47:14 +0100

    Updated.

--- node/pinclude/lux/communication/NodeGroup.hpp
+++ node/pinclude/lux/communication/NodeGroup.hpp
@@ -167,11 +167,6 @@ namespace lux::communication
 			action.type = ENodeActionType::Pinged;
 			action.info = _self_info;
 
-
-			while(_running)
-			{
-				_multicast->send(reinterpret_cast<void*>(&action), sizeof(action));
-				std::this_thread::sleep_for(std
 			while(_running)
 			{
 				_multicast->send(reinterpret_cast<void*>(&action), sizeof(action));
