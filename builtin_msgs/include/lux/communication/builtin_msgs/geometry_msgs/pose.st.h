bc594ba1be911b15a8fd603fe3cb0048e2238f9f
chenhui.lux.yu <chenhui.lux.yu@outlook.com>
2025-01-30 11:58:43 +0000

    Updated.

--- builtin_msgs/include/lux/communication/builtin_msgs/geometry_msgs/pose.st.h
+++ builtin_msgs/include/lux/communication/builtin_msgs/geometry_msgs/pose.st.h
@@ -8,5 +8,13 @@ namespace lux::communication::builtin_msgs::geometry_msgs
 	{
 		PointS		position;
 		QuaternionS	orientation;
+
+		Eigen::Matrix4d SE3()
+		{
+			Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
+			mat.block<3, 1>(0, 3) = position;
+			mat.block<3, 3>(0, 0) = orientation.toRotationMatrix();
+			return mat;
+		}
 	};
 }
