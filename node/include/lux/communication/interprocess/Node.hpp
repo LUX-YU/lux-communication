6453c39947fd7a3451c7d2e2a74ff9dee773a1a5
chenhui.lux.yu <113639156+LUX-YU@users.noreply.github.com>
2025-05-23 21:44:10 +0100

    Add spin support and tests

--- node/include/lux/communication/interprocess/Node.hpp
+++ node/include/lux/communication/interprocess/Node.hpp
@@ -5,9 +5,11 @@
 
 #include <vector>
 #include <functional>
+#include <atomic>
 
 #include <lux/communication/CallbackGroup.hpp>
 #include <lux/communication/interprocess/ZmqPubSub.hpp>
+#include <lux/communication/Executor.hpp>
 #include <lux/communication/visibility.h>
 
 namespace lux::communication::interprocess {
@@ -19,6 +21,7 @@ public:
     {
         default_group_ = std::make_shared<lux::communication::CallbackGroup>(
             lux::communication::CallbackGroupType::MutuallyExclusive);
+        callback_groups_.push_back(default_group_);
     }
 
     ~Node() { stop(); }
@@ -35,27 +38,66 @@ public:
     }
 
     template<typename T, typename Callback>
-    std::shared_ptr<Subscriber<T, std::decay_t<Callback>>> createSubscriber(
+    std::shared_ptr<Subscriber<T>> createSubscriber(
         const std::string& topic, Callback&& cb,
         std::shared_ptr<lux::communication::CallbackGroup> group = nullptr)
     {
-        (void)group;
-        auto sub = std::make_shared<Subscriber<T, std::decay_t<Callback>>>(
-            topic, std::forward<Callback>(cb));
+        if (!group)
+        {
+            group = default_group_;
+        }
+        auto sub = std::make_shared<Subscriber<T>>(topic, std::forward<Callback>(cb), group, next_sub_id_++);
+        if (group)
+        {
+            group->addSubscriber(sub.get());
+            bool exists = false;
+            for (auto& g : callback_groups_)
+            {
+                if (g == group)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists)
+            {
+                callback_groups_.push_back(group);
+            }
+        }
         subscriber_stoppers_.emplace_back([s=sub]{ s->stop(); });
         return sub;
     }
 
     void stop()
     {
+        auto exec = spinning_exec_.load();
+        if (exec)
+        {
+            exec->stop();
+        }
         for (auto& fn : subscriber_stoppers_) { fn(); }
         subscriber_stoppers_.clear();
+    }
+
+    void spin()
+    {
+        auto exec = std::make_shared<lux::communication::SingleThreadedExecutor>();
+        for (auto& g : callback_groups_)
+        {
+            exec->addCallbackGroup(g);
+        }
+        spinning_exec_.store(exec.get(), std::memory_order_release);
+        exec->spin();
+        spinning_exec_.store(nullptr, std::memory_order_release);
     }
 
 private:
     std::string name_;
     std::shared_ptr<lux::communication::CallbackGroup> default_group_;
+    std::vector<std::shared_ptr<lux::communication::CallbackGroup>> callback_groups_;
     std::vector<std::function<void()>> subscriber_stoppers_;
+    std::atomic<lux::communication::Executor*> spinning_exec_{nullptr};
+    int next_sub_id_{0};
 };
 
 } // namespace lux::communication::interprocess
